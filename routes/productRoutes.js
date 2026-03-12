6162fc27bf26c4f0489265216d56db39d07aafa4
Deepak varshney <manojdeepak145@gmail.com>
2026-03-12 15:04:33 +0530

    fix route order

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,11 +5,12 @@ const controller = require("../controllers/productController");
 
 router.post("/products", controller.addProduct);
 router.get("/products", controller.getProducts);
-router.get("/products/:id", controller.getProductById);
-router.put("/products/:id", controller.updateProduct);
-router.delete("/products/:id", controller.deleteProduct);
 
 router.get("/products/search", controller.searchProduct);
 router.get("/products/category", controller.categoryFilter);
+
+router.get("/products/:id", controller.getProductById);
+router.put("/products/:id", controller.updateProduct);
+router.delete("/products/:id", controller.deleteProduct);
 
 module.exports = router;
